0ec50625f30a29935dfdd99b58546fd7f146c122
Patrik Hedman <patrik@moresale.se>
2009-07-21 23:50:12 +0200

    setup test env for activerecord models

--- test/access_denied_test.rb
+++ test/access_denied_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class AccessDeniedTest < ActiveSupport::TestCase
+  load_schema
+
+  class User < ActiveRecord::Base; end
+
+  test "activerecord stuff" do
+    assert_equal [], User.all
+  end
 end
